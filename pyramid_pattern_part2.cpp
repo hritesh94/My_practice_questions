9c09922565246cf56d73b3837f07ff748184868b
hritesh94 <hritesh94.4@gmail.com>
2022-06-03 17:15:14 +0530

    correction

--- pyramid_pattern_part2.cpp
+++ pyramid_pattern_part2.cpp
@@ -16,49 +16,65 @@ int main()
         while (i < n) 
         { 
             int j = 1 ; 
-            while (j<n-i) 
-            { 
-                cout <<"  "; 
-                j++;
-                
-            }
+                while (j<n-i) 
+                    { 
+                        cout <<" "<<" "; 
+                        j++;
+                        
+                    }
             //cout<<endl;
             //Here this was a terrible mistake where if we put endline before even reaching the next line of code the desired pattern will not be received 
            int k =1;
             while(k<=i)
-            
-            { 
-                cout <<" *"; 
-                k++; 
-            } 
+                
+                { 
+                    cout <<"*"<<" "; 
+                    k++; 
+                } 
             // here we initialised the third column of pyramid from 2 because if we initialise from l=1 it creates a duplicate 180 inverted half pyramid, we want that but the point above 
             // becomes blunt so to make it sharp we initialise from 2 and as its while loop so its entry condition loop so when i becomes 2 it prints half pyramid but small one which completes the full big pyramid
             int l =2;
             while(l<=i)
             
-            { 
-                cout <<" *"; 
-                l++; 
-            } 
-           
-            int g =1;
-            while(g<=n-i)
-            {
-                cout<<" ";
-                g++;
-            }
-
-             int h =1;
-            while(h<=i)
-            {
-                cout<<" *";
-                h++;
-            }
-
-
-
+                { 
+                    cout <<"*"<<" "; 
+                    l++; 
+                } 
+            
             cout<<endl;
             i++; 
+        }
+
+
+        int i2=1;
+
+        while(i2<=n)
+        {
+            int g =2;
+            while(g<=i2)
+                    {
+                        cout<<" "<<" ";
+                        g++;
+                    }
+
+            int h =1;
+            while(h<=n-i2)
+                    {
+                        cout<<"*"<<" ";
+                        h++;
+                    }
+                   
+
+             int o =2;
+            while(o<=n-i2)
+                    {
+                        cout<<"*"<<" ";
+                        o++;
+                    }
+             cout<<endl;
+                    i2++;
+
+
         }
         return 0;
         
