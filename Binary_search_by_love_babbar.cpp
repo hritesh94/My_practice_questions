e14ad32d721dc0885c98dd5872571c0479d25287
hritesh94 <hritesh94.4@gmail.com>
2022-06-05 20:32:49 +0530

    binary search optimized

--- Binary_search_by_love_babbar.cpp
+++ Binary_search_by_love_babbar.cpp
@@ -6,8 +6,11 @@ int binarySearch(int arr[],int size,int key)
     {
         int start = 0, end = size - 1;
         
-        int mid = (start+end)/2;
-        
+       // int mid = (start+end)/2;
+        /*Here above we can use this formula but the problem is that suppose if there is an array index which has index number 2^(-31) so 
+        if it is used in that formula above which can create a problem of out of range for integer data type 
+        so we use another formula which can avoid this problem below:- */
+        int mid = start + (end-start)/2;
         while(start <= end)
             {
                 if(arr[mid] == key)
@@ -22,7 +25,7 @@ int binarySearch(int arr[],int size,int key)
                     {
                         end = mid - 1;
                     }
-                mid = (start+end)/2;
+               mid = start + (end-start)/2;
             }
         
         return -1;
