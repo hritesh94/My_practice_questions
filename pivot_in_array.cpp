85fc26b4e9e2872805f4e6c3f050f65a2b3f32d0
hritesh94 <hritesh94.4@gmail.com>
2022-06-09 22:33:16 +0530

    actual pivot program
    could not understand it
    will do rest tomorrow

--- pivot_in_array.cpp
+++ pivot_in_array.cpp
@@ -1,47 +1,36 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int binarySearch(int arr[],int key,int size)
-{
-int start = 0;
-int end = size-1;
 
-int mid = start + (end-start)/2;
-while (start<=end)
-{
-    if(arr[mid]==key)
+int getPivot(int arr[],int n)
     {
-        return mid;
+        int s = 0;
+        int e = n - 1;
+        int mid = s + (e-s)/2;
+        
+        while(s<e){
+            if(arr[mid] >= arr[0])
+            {
+                s = mid + 1;
+            }
+        else{
+                e = mid;
+        }
+        mid = s + (e-s)/2;
     }
-else if(arr[mid]>key)
-{
-end = mid -1;
+    return s;
+   }
+
+int main() {
+    
+    int arr[5] = {3,8,10,17,1};
+    
+    cout<<"Pivot is:"<<getPivot(arr,5)<<endl;
+
+    return 0;
 }
-else
-{
-    start = mid + 1;
-}
 
-mid = start + (end-start)/2;
-}
-return -1;
-
-
-
-}
-int main()
-    {
-        int arr[5] = {1,2,3,4,5};
-
-        int keyIndex = binarySearch(arr, 2,5);
-
-        cout<<"The index of number 2:"<<keyIndex;
-
-        return 0;
-
-    }
-
-//just a revision of binary search
-// nothing else 
+//started with pivot from love babbar but couldnt understand 
+//and i dont have enough energy to understand 
 // will do rest thing tomorrow
 // just did not wanted the code streak to break 
 //crey emoji
