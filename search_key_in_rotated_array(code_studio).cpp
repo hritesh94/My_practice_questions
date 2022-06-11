cc5407162f0659f792a624da5212c7b5849dcc11
hritesh94 <hritesh94.4@gmail.com>
2022-06-11 22:34:39 +0530

    love babbar_pivot_array

---
+++ search_key_in_rotated_array(code_studio).cpp
@@ -0,0 +1,58 @@
+int getPivot(vector<int>& arr,int n)
+    {
+        int s = 0;
+        int e = n - 1;
+        int mid = s + (e-s)/2;
+        
+        while(s<e){
+            if(arr[mid] >= arr[0])
+            {
+                s = mid + 1;
+            }
+        else{
+                e = mid;
+        }
+        mid = s + (e-s)/2;
+    }
+    return s;
+   }
+
+int binarySearch(vector<int>& arr,int s,int e ,int key)
+    {
+        int start = s, end = e;
+        
+      
+        int mid = start + (end-start)/2;
+        while(start <= end)
+            {
+                if(arr[mid] == key)
+                    {
+                        return mid;
+                    }
+                else if(key > arr[mid])
+                    {
+                        start = mid + 1;
+                    }
+                else 
+                    {
+                        end = mid - 1;
+                    }
+               mid = start + (end-start)/2;
+            }
+        
+        return -1;
+    }
+
+int findPosition(vector<int>& arr, int n, int k)
+{
+     int pivot = getPivot(arr,n);
+    if(k >= arr[pivot] && k<=arr[n-1])
+    {//Binary search on first line 
+        return binarySearch(arr,pivot, n-1,k);
+    }
+    else
+    {// Binary search on 2nd line
+        return binarySearch(arr,0,pivot - 1, k);
+    }
+}
+
